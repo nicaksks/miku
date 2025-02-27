da43090488e6539cb3f1262e95e81ce679dd7eab
nicaksks <juliesuzuya@outlook.com>
2025-02-27 03:49:39 -0300

    ૮ ˶ᵔ ᵕ ᵔ˶ ა

--- miku/Miku.ts
+++ miku/Miku.ts
@@ -1,12 +1,14 @@
 import type { Options } from "@miku/types/Miku";
 import Channel from "./Channel";
+import MikuError from "./errors/MikuError";
 
 export default class Miku {
 
     private static options: Options
 
-    public constructor(opts: Options) {
-        Miku.options = opts
+    public constructor(private readonly opts: Options) {
+        if(!this.opts.token) throw new MikuError({ code: 40001, type: 'Authentication', message: 'Authentication failed', error_details: 'Unauthorized. Provide a valid token and try again'})
+        Miku.options = this.opts
     }
 
     public static get guild_id(): string {
