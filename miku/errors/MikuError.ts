64d09855242d580a49bf296f9ea2bfb993d84be0
nica <100009274+nicaksks@users.noreply.github.com>
2025-03-02 19:47:10 -0300

    Update MikuError.ts

--- miku/errors/MikuError.ts
+++ miku/errors/MikuError.ts
@@ -2,14 +2,14 @@ interface CustomError {
     readonly code: number;
     readonly type: string;
     readonly message: string;
-    readonly error_details?: any
+    readonly error_details?: string | Array<string> | null
 }
 
 export default class MikuError extends Error implements CustomError {
 
     readonly code: number;
     readonly type: string;
-    readonly error_details?: any;
+    readonly error_details?: string | Array<string> | null;
 
     public constructor({ code, type, message, error_details = null }: CustomError) {
         super()
